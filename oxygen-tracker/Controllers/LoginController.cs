8f755e8c53982f7364cfb698f5aea237093651d2
amal <arj060892@gmail.com>
2021-04-18 23:40:11 +0530

    Added Authentication and auth for user registration

---
+++ oxygen-tracker/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace oxygen_tracker.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetSecuredData()
+        {
+            return Ok("All good");
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administration")]
+        public async Task<IActionResult> PostSecuredData()
+        {
+            return Ok("Posted correctly");
+        }
+    }
+}
---
+++ oxygen-tracker/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using oxygen_tracker.Controllers.Services;
+using oxygen_tracker.Models;
+using System.Threading.Tasks;
+
+namespace oxygen_tracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            this._userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync(RegisterModel model)
+        {
+            var result = await _userService.RegisterAsync(model);
+            return Ok(result);
+        }
+
+        [HttpPost("addrole")]
+        public async Task<IActionResult> AddRoleAsync(AddRoleModel model)
+        {
+            var result = await _userService.AddRoleAsync(model);
+            return Ok(result);
+        }
+    }
+}
